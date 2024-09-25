82e3003171a6104f6bb4a39222a546f16357cd71
hoken92 <hkenneth92@gmail.com>
2024-09-24 19:06:11 -0700

    copy array and increment

--- script.js
+++ script.js
@@ -122,12 +122,33 @@ let avgAgeOfArray = sumOfNewArray / newArray.length;
 console.log(avgAgeOfArray);
 
 // ============================================================
-/**
- * Part 3: Thinking Critically
-It is important to remember that when working with objects in JavaScript, we can either pass those objects into functions by value or by reference. This important distinction changes the way that functions behave, and can have large impacts on the way a program executes.
-For this section, develop functions that accomplish the following:
-Take an object and increment its age field.
-Take an object, make a copy, and increment the age field of the copy. Return the copy.
-For each of the functions above, if the object does not yet contain an age field, create one and set it to 0. Also, add (or modify, as appropriate) an updated_at field that stores a Date object with the current time.
-Thought experiment: since the Date object is an object, what would happen if we modified it in the copy of the object created in the second function using setTime() or another method? How could we circumvent potentially undesired behavior?
- */
+// Part 3: Thinking Critically
+
+// For this section, develop functions that accomplish the following:
+// Take an object and increment its age field.
+function incrementObjectAge(array, n) {
+  array.map(function (person) {
+    person.age += n;
+    return person;
+  });
+  return array;
+}
+console.log("Take an object and increment its age field.");
+console.log(incrementObjectAge(newArray, 6));
+
+// Take an object, make a copy, and increment the age field of the copy. Return the copy.
+function copyObjectAndAdd(originalArr, n) {
+  let copyOfArray = JSON.parse(JSON.stringify(originalArr));
+  copyOfArray.map(function (person) {
+    person.age += n;
+    return person;
+  });
+  return copyOfArray;
+}
+console.log(
+  "Take an object, make a copy, and increment the age field of the copy. Return the copy."
+);
+console.log(copyObjectAndAdd(newArray, 5));
+
+// For each of the functions above, if the object does not yet contain an age field, create one and set it to 0. Also, add (or modify, as appropriate) an updated_at field that stores a Date object with the current time.
+// Thought experiment: since the Date object is an object, what would happen if we modified it in the copy of the object created in the second function using setTime() or another method? How could we circumvent potentially undesired behavior?
